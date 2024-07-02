b9867da59c5a3387c2a71fec58fc93c931b4cf05
elena-dogman <dogman.choli@gmail.com>
2024-07-02 11:19:25 +0300

    feat: add search component and result

--- class-components/src/components/App/App.tsx
+++ class-components/src/components/App/App.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import './App.module.scss';
-import StarsBackground from '../StarsBackground/StarsBackground';
+import styles from'./App.module.scss';
+import SearchBar from '../SearchBar/SearchBar';
+import Results from '../Results/Results';
 
 class App extends React.Component {
+  handleSearch = (term: string) => {
+    console.log('Perform search with term:', term);
+  }
+
   render() {
     return (
-      <div className="app">
-        <StarsBackground />
-        <h1>It's a wrapper</h1>
+      <div className={styles.app}>
+           <header className="header">
+          <SearchBar onSearch={this.handleSearch} />
+          </header>
+        <Results />
       </div>
     );
   }
 }
+
 export default App;
