1182ffb0a9f898b42ae3bbe9810dc3576688acee
elena-dogman <dogman.choli@gmail.com>
2024-07-03 20:04:50 +0300

    feat: add getting data from api functionality

--- class-components/src/components/App/App.tsx
+++ class-components/src/components/App/App.tsx
@@ -3,9 +3,35 @@ import styles from './App.module.scss';
 import SearchBar from '../SearchBar/SearchBar';
 import Results from '../Results/Results';
 
-class App extends React.Component {
+interface Character {
+  name: string;
+  gender: string;
+  height: string;
+  eye_color: string;
+  url: string;
+}
+
+interface AppState {
+  characters: Character[];
+}
+
+class App extends React.Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      characters: [],
+    };
+  }
+
   handleSearch = (term: string) => {
-    console.log('Perform search with term:', term);
+    fetch(`https://swapi.dev/api/people/?search=${term}`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ characters: data.results });
+      })
+      .catch(error => {
+        console.error('Error fetching characters:', error);
+      });
   };
 
   render() {
@@ -14,7 +40,7 @@ class App extends React.Component {
         <header className={styles.header}>
           <SearchBar onSearch={this.handleSearch} />
         </header>
-        <Results />
+        <Results characters={this.state.characters} />
       </div>
     );
   }
--- class-components/src/components/Results/Results.tsx
+++ class-components/src/components/Results/Results.tsx
@@ -1,14 +1,43 @@
 import React from 'react';
 import styles from './Results.module.scss';
 
-class Results extends React.Component {
+interface Character {
+  name: string;
+  gender: string;
+  height: string;
+  eye_color: string;
+  url: string;
+}
+
+interface ResultsProps {
+  characters: Character[];
+}
+
+class Results extends React.Component<ResultsProps> {
   render() {
-    return <div className={styles.results}>
-      <img src="src\assets\r2d2.png" className={styles["character-image"]}></img>
-      <div className={styles["character-info"]}>
-        <p className={styles["character-name"]}></p>
+    const { characters } = this.props;
+
+    if (!characters.length) {
+      return <div className={styles.results}>No character data available</div>;
+    }
+
+    return (
+      <div className={styles.results}>
+        <img
+          src="src/assets/r2d2.png"
+          className={styles['character-image']}
+          alt="R2D2"
+        />
+        {characters.map((character) => (
+          <div key={character.url} className={styles.character}>
+            <h2>{character.name}</h2>
+            <p><strong>Gender:</strong> {character.gender}</p>
+            <p><strong>Height:</strong> {character.height}</p>
+            <p><strong>Eye Color:</strong> {character.eye_color}</p>
+          </div>
+        ))}
       </div>
-    </div>;
+    );
   }
 }
 
