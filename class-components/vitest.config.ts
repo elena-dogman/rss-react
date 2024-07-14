fea392ddc1f08a37c8180926dec7f2fea391bcd5
elena-dogman <dogman.choli@gmail.com>
2024-07-14 10:44:46 +0300

    feat: update vitest config

---
+++ class-components/vitest.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig, coverageConfigDefaults } from 'vitest/config';
+import { resolve } from 'node:path';
+
+export default defineConfig({
+  resolve: {
+    alias: [{find: '@', replacement: resolve(__dirname, './src')}],
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['src/__tests__/setup.jsdom.ts'],
+    coverage: {
+      all: true,
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      reportsDirectory: './tests/unit/coverage',
+      include: ['src/**'],
+      exclude: [...coverageConfigDefaults.exclude, 'src/main.tsx', 'src/**/*.d.ts'],
+    },
+    css: false,
+  },
+});
