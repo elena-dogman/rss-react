2e5e37dcf21e53767d15528ddcdd1692ca52f42e
elena-dogman <dogman.choli@gmail.com>
2024-07-14 20:20:24 +0300

    feat: write tests for character detail

---
+++ class-components/src/components/CharacterDetails/__tests__/CharacterDetail.test.tsx
@@ -0,0 +1,55 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { describe, it, expect, vi } from 'vitest';
+import CharacterDetails, { DetailedCharacter } from '../CharacterDetails';
+
+vi.mock('../../Loader/Loader', () => ({
+  __esModule: true,
+  default: () => <div>Mocked Loader</div>,
+}));
+
+vi.mock('../../CloseButton/CloseButton', () => ({
+  __esModule: true,
+  default: ({ onClick }: { onClick: () => void }) => (
+    <button onClick={onClick}>Mocked Close Button</button>
+  ),
+}));
+
+const character: DetailedCharacter = {
+  name: 'Luke Skywalker',
+  birth_year: '19BBY',
+  gender: 'male',
+  height: '172',
+  mass: '77',
+  eye_color: 'blue',
+  skin_color: 'fair',
+  homeworld: 'Tatooine',
+  url: 'http://swapi.dev/api/people/1/',
+};
+
+describe('CharacterDetails Component', () => {
+  it('should display a loading indicator while fetching data', () => {
+    render(<CharacterDetails character={null} isLoading={true} onClose={vi.fn()} homeworld="" />);
+    expect(screen.getByText('Mocked Loader')).toBeInTheDocument();
+  });
+
+  it('should correctly display the detailed card data', () => {
+    render(<CharacterDetails character={character} isLoading={false} onClose={vi.fn()} homeworld="Tatooine" />);
+    expect(screen.getByText(character.name)).toBeInTheDocument();
+    expect(screen.getByText((content, element) => element?.textContent === `Birth Year: ${character.birth_year}`)).toBeInTheDocument();
+    expect(screen.getByText((content, element) => element?.textContent === `Gender: ${character.gender}`)).toBeInTheDocument();
+    expect(screen.getByText((content, element) => element?.textContent === `Height: ${character.height}`)).toBeInTheDocument();
+    expect(screen.getByText((content, element) => element?.textContent === `Mass: ${character.mass}`)).toBeInTheDocument();
+    expect(screen.getByText((content, element) => element?.textContent === `Eye Color: ${character.eye_color}`)).toBeInTheDocument();
+    expect(screen.getByText((content, element) => element?.textContent === `Skin Color: ${character.skin_color}`)).toBeInTheDocument();
+    expect(screen.getByText((content, element) => element?.textContent === `Homeworld: Tatooine`)).toBeInTheDocument();
+  });
+
+  it('should hide the component when clicking the close button', () => {
+    const onCloseMock = vi.fn();
+    render(<CharacterDetails character={character} isLoading={false} onClose={onCloseMock} homeworld="Tatooine" />);
+    const closeButton = screen.getByText('Mocked Close Button');
+    fireEvent.click(closeButton);
+    expect(onCloseMock).toHaveBeenCalled();
+  });
+});
