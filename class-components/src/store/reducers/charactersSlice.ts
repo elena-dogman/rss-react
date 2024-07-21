6a134aad94a99196daf970aad7b938fd51813513
elena-dogman <dogman.choli@gmail.com>
2024-07-21 10:40:58 +0300

    feat: create characters slice

---
+++ class-components/src/store/reducers/charactersSlice.ts
@@ -0,0 +1,43 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Character } from "../../api/characters";
+
+interface CharacterState {
+  characters: Character[];
+  homeworlds: { [url: string]: string };
+  currentPage: number;
+  totalPages: number;
+  isLoading: boolean;
+}
+
+const initialState: CharacterState = {
+  characters: [],
+  homeworlds: {},
+  currentPage: 1,
+  totalPages: 1,
+  isLoading: false,
+}
+
+const characterSlice = createSlice({
+  name: 'characters',
+  initialState,
+  reducers: {
+    setCharacters(state, action: PayloadAction<Character[]>) {
+      state.characters = action.payload;
+    },
+    setHomeworlds(state, action: PayloadAction<{ [url: string]: string }>) {
+      state.homeworlds = action.payload;
+    },
+    setCurrentPage(state, action: PayloadAction<number>) {
+      state.currentPage = action.payload;
+    },
+    setTotalPages(state, action: PayloadAction<number>) {
+      state.totalPages = action.payload;
+    },
+    setIsLoading(state, action: PayloadAction<boolean>) {
+      state.isLoading = action.payload;
+    }
+  }
+});
+
+export const { setCharacters, setHomeworlds, setCurrentPage, setTotalPages, setIsLoading } = characterSlice.actions;
+export default characterSlice.reducer;
