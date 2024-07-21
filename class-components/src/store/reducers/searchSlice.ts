a84c6b4c24256d76dc1e37356ff47bd774fbe4f0
elena-dogman <dogman.choli@gmail.com>
2024-07-21 10:30:20 +0300

    feat: set up store and create search reducer

---
+++ class-components/src/store/reducers/searchSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface SearchState {
+  searchTerm: string;
+}
+
+const initialState: SearchState = {
+  searchTerm: '',
+}
+
+const searchSlice = createSlice({
+  name: "search",
+  initialState,
+  reducers: {
+    setSeachTerm(state, action: PayloadAction<string>) {
+      state.searchTerm = action.payload;
+    }
+  }
+});
+
+export const { setSeachTerm } = searchSlice.actions;
+export default searchSlice.reducer
