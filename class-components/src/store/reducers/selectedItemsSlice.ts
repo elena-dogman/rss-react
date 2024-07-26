22f1ae3a15d4d245a571bd116007fc996b44bc33
elena-dogman <dogman.choli@gmail.com>
2024-07-26 14:02:41 +0300

    feat: update selected items slice

--- class-components/src/store/reducers/selectedItemsSlice.ts
+++ class-components/src/store/reducers/selectedItemsSlice.ts
@@ -5,9 +5,31 @@ interface SelectedItemsState {
   selectedItems: { [url: string]: Character };
 }
 
-const initialState: SelectedItemsState = {
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('selectedItems');
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    console.error("Could not load state", err);
+    return undefined;
+  }
+};
+
+const saveState = (state: SelectedItemsState) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('selectedItems', serializedState);
+  } catch (err) {
+    console.error("Could not save state", err);
+  }
+};
+
+const initialState: SelectedItemsState = loadState() || {
   selectedItems: {},
-}
+};
 
 const selectedItemsSlice = createSlice({
   name: 'selectedItems',
@@ -15,12 +37,15 @@ const selectedItemsSlice = createSlice({
   reducers: {
     addItem: (state, action: PayloadAction<Character>) => {
       state.selectedItems[action.payload.url] = action.payload;
+      saveState(state);
     },
     removeItem: (state, action: PayloadAction<string>) => {
       delete state.selectedItems[action.payload];
+      saveState(state);
     },
     clearItems: (state) => {
       state.selectedItems = {};
+      saveState(state);
     }
   },
 });
